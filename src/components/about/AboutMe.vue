652d37313e6f270af667aefd6d8b0f8de106ca00
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-07-23 12:45:17 +0200

    TheHolyDance

---
+++ src/components/about/AboutMe.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  setup() {
+    return {
+      bios: [
+        {
+          id: 1,
+          bio: "👋 Hi, I’m @Bwirabuciza29 👀 I’m interested in creating intuitive and visually appealing user interfaces, solving design challenges, and exploring new design trends. 🌱 I’m currently learning advanced animation techniques and mastering component-based architecture in Vue.js. 💞️ I’m looking to collaborate on projects that push the boundaries of UI/UX design, where I can contribute my expertise in Vue.js development. 📫 How to reach me: You can reach me via outlook at achilleblondy@outlook.fr or connect with me on LinkedIn https://www.linkedin.com/in/bwirabuciza-blondy-58b137245. 😄 Pronouns: He/Him ⚡ Fun fact: I'm an avid hiker and find inspiration for my designs in nature's patterns and colors.",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+    <!-- About profile image -->
+    <div class="w-full sm:w-1/4 mb-7 sm:mb-0">
+      <img src="@/assets/images/blondy.jpg" class="rounded-xl w-96" alt="" />
+    </div>
+
+    <!-- About details -->
+    <div class="w-full sm:w-3/4 text-left">
+      <p
+        v-for="bio in bios"
+        :key="bio.id"
+        class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg text-justify"
+      >
+        {{ bio.bio }}
+      </p>
+    </div>
+  </div>
+</template>
---
+++ src/data/socialLinks.js
@@ -0,0 +1,27 @@
+export const socialLinks = [
+	// {
+	// 	id: 1,
+	// 	name: 'Website',
+	// 	icon: 'globe',
+	// 	url: 'https://www.stoman.me/',
+	// },
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/Bwirabuciza29',
+	},
+	{
+		id: 3,
+		name: 'Twitter',
+		icon: 'twitter',
+		url: 'https://x.com/achille_blondy?t=EN2JnTtQGFZkRu3m0Mp1lw&s=09',
+	},
+	{
+		id: 4,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/bwirabuciza-blondy-58b137245?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+	},
+	
+];
